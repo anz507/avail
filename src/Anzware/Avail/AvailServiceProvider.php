5ed951de4ca8969eb081a870c795adfe75f8d132
Ahmad Anshori <anz507@gmail.com>
2016-12-14 15:06:43 +0800

    Initial commit

---
+++ src/Anzware/Avail/AvailServiceProvider.php
@@ -0,0 +1,53 @@
+<?php namespace Anzware\Avail;
+
+use Illuminate\Support\ServiceProvider;
+
+class AvailServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('anzware/avail');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app['avail'] = $this->app->share(function($app)
+		{
+		    return new Avail;
+		});
+
+		$this->app->booting(function()
+		{
+		  	$loader = \Illuminate\Foundation\AliasLoader::getInstance();
+		  	$loader->alias('Avail', 'Anzware\Avail\Facades\Avail');
+		});
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('avail');
+	}
+
+}
