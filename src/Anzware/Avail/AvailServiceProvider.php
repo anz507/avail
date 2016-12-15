c131206b23644014b534198e242c5d7bdeadea91
Ahmad Anshori <anz507@gmail.com>
2016-12-15 11:30:36 +0800

    Include route file to AvailServiceProvider

--- src/Anzware/Avail/AvailServiceProvider.php
+++ src/Anzware/Avail/AvailServiceProvider.php
@@ -4,50 +4,52 @@ use Illuminate\Support\ServiceProvider;
 
 class AvailServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('anzware/avail');
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('anzware/avail');
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app['avail'] = $this->app->share(function($app)
-		{
-		    return new Avail;
-		});
+        include __DIR__.'/../../routes.php';
+    }
 
-		$this->app->booting(function()
-		{
-		  	$loader = \Illuminate\Foundation\AliasLoader::getInstance();
-		  	$loader->alias('Avail', 'Anzware\Avail\Facades\Avail');
-		});
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['avail'] = $this->app->share(function($app)
+        {
+            return new Avail;
+        });
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('avail');
-	}
+        $this->app->booting(function()
+        {
+            $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+            $loader->alias('Avail', 'Anzware\Avail\Facades\Avail');
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('avail');
+    }
 
 }
