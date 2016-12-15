1882b6cc18bcc7094f3284740a4bae414b2c1d55
Ahmad Anshori <anz507@gmail.com>
2016-12-15 11:30:59 +0800

    Change tabs to spaces

--- src/Anzware/Avail/Facades/Avail.php
+++ src/Anzware/Avail/Facades/Avail.php
@@ -3,12 +3,10 @@
 use Illuminate\Support\Facades\Facade;
 
 class Avail extends Facade {
-
-  /**
-   * Get the registered name of the component.
-   *
-   * @return string
-   */
-  protected static function getFacadeAccessor() { return 'avail'; }
-
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+    */
+    protected static function getFacadeAccessor() { return 'avail'; }
 }
