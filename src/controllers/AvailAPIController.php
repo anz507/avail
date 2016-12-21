cdd206f85489e722572214e43826237f19a20b7f
Ahmad Anshori <anz507@gmail.com>
2016-12-21 12:56:14 +0800

    Add state from booking table to the response

--- src/controllers/AvailAPIController.php
+++ src/controllers/AvailAPIController.php
@@ -1,27 +1,55 @@
 <?php namespace Anzware\Avail;
+/**
+ * All in one API for Available Calendar
+ *
+ */
 
-use \Illuminate\Routing\Controller;
+use Illuminate\Routing\Controller;
 use Anzware\Avail\Helper\AvailResponse;
-use \Carbon\Carbon;
+use Carbon\Carbon;
+use Anzware\Avail\AVBooking;
+use Anzware\Avail\AVCalendar;
+use Anzware\Avail\AVState;
+use Input;
+use Validator;
 
 class AvailAPIController extends Controller
 {
+    /**
+     * Method for getting the calendar
+     *
+     * @param integer    `take`    (optional)    - the amount of months needed to be displayed (default: 3)
+     * @author Ahmad Anshori <anz507@gmail.com>
+     *
+     * @todo implement pagination
+     */
     public function getCalendar()
     {
         try {
+            $take = Input::get('take', 3);
+            $calendarId = Input::get('calendar_id');
+
+            // @todo: add validator here
+
             $calendar = array();
 
             $currentDate = Carbon::now();
             $currentDateStartOfDay = Carbon::now()->startOfDay();
             $currentDateStartOfMonth = Carbon::now()->startOfMonth();
+            $endDateEndOfMonth = Carbon::now()->startOfMonth()->addMonths($take-1)->endOfMonth();
 
-            // the amount of months needed to be displayed
-            $take = 3;
+            // get all bookings within taken months
+            $bookings = AVBooking::with('state')
+                ->where('calendar_date', '>=', $currentDateStartOfMonth)
+                ->where('calendar_date', '<=', $endDateEndOfMonth)
+                ->get();
 
-            for ($i = 0; $i < $take; $i++) {
-                $month = Carbon::now()->startOfMonth()->addMonths($i);
+            for ($m = 0; $m < $take; $m++) {
+                $month = Carbon::now()->startOfMonth()->addMonths($m);
                 $monthData = new \stdclass();
                 $monthData->name = $month->format('F');
+                $monthData->month = $month->month;
+                $monthData->year = $month->year;
                 $monthData->days = array();
 
                 for ($d = 1; $d <= $month->daysInMonth; $d++) {
@@ -29,7 +57,7 @@ class AvailAPIController extends Controller
                     $day->day = $d;
                     $day->month = $month->month;
                     $day->year = $month->year;
-                    $day->stringDate = Carbon::now()->startOfMonth()->addMonths($i)->addDays($d-1)->toDateString();
+                    $day->stringDate = Carbon::now()->startOfMonth()->addMonths($m)->addDays($d-1)->toDateString();
                     // check for today date
                     if ($currentDate->day === $d &&
                         $currentDate->month === $month->month &&
@@ -38,10 +66,20 @@ class AvailAPIController extends Controller
                         // set the 'current' flag
                         $day->current = TRUE;
                     }
+
+                    $day->state = 'Available';
+
+                    // override state from bookings table
+                    foreach ($bookings as $booking) {
+                        if ($day->stringDate === $booking->calendar_date) {
+                            $day->state = $booking->state->state;
+                        }
+                    }
+
                     $monthData->days[] = $day;
                 }
 
-                $calendar[$i] = $monthData;
+                $calendar[$m] = $monthData;
             }
 
             $data = new AvailResponse();
